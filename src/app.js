94f85103152f022daecb78ef42ef0a304c57130e
ShaderFax <westleyhowe@hotmail.com>
2025-11-26 12:49:42 -0600

    reworking everything

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import './src/app.html';
+
+
+function wheel() {
+    /* When adding and removing items
+    use the math function [100% / num of items]
+    to have each segment be equal*/
+}
+
+function wheelSpin() {
+    
+}
+
+function serializeList() {
+    /*when an item is added, you need to push the item to a json to be pulled back later */
+}
+
+function deserializeList() {
+    /* when an item is removed, it needs to be removed from the json file */
+}
